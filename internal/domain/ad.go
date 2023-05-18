a22671a704b8c1cd8ae7a8aa266065303f7435bb
juan.ibars <juan.ibars@adevinta.com>
2023-05-18 19:51:07 +0200

    refactor entity

--- internal/domain/ad.go
+++ internal/domain/ad.go
@@ -14,21 +14,6 @@ type Ad struct {
 	Date        time.Time
 }
 
-func GenerateUUID() uuid.UUID {
-	result, _ := uuid.NewRandom()
-	return result
-}
-
-func CreateAd(title string, description string, price float64) Ad {
-	return Ad{
-		Id:          GenerateUUID(),
-		Title:       title,
-		Description: description,
-		Price:       price,
-		Date:        time.Now(),
-	}
-}
-
 func (ad Ad) Print() {
 	fmt.Printf("ad:{\n id:%s\n title:%s\n description:%s\n price:%f\n date:%s\n}\n", ad.Id.String(), ad.Title, ad.Description, ad.Price, ad.Date.String())
 }
